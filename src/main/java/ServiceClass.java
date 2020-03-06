cd99e86646de75a56df6cc5cb507f67a5761f5a6
dwr <damian.wrobel.1990@gmail.com>
2020-03-06 20:23:20 +0100

    Serwis

--- src/main/java/ServiceClass.java
+++ src/main/java/ServiceClass.java
@@ -5,19 +5,23 @@ import entity.Driver;
 import java.util.List;
 import java.util.Optional;
 
-public class MainClass {
+public class ServiceClass {
 
     public static void main(String[] args) {
 
         DriverDAOInterface<Driver> driverDAO = new DriverDAO();
 
         Optional driver = driverDAO.get(1L);
+        System.out.println("\n");
 
         driver.ifPresent(value -> System.out.println(((Driver)value).getFirstName()));
+        System.out.println("\n");
 
-        driverDAO.save(new Driver("Kamil", "Turek"));
+        driverDAO.save(new Driver("Jerzy", "Turek"));
+        System.out.println("\n");
 
         List<Driver> turek = driverDAO.getDriverbyLastName("Turek");
+        System.out.println("\n");
 
         if(turek.size()>0){
             System.out.println("Pobrano poprzez DriverDAO");
